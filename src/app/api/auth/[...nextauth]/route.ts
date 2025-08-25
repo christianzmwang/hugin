dfaf50d0c63f955a7de07286137444abfc97cf35
Christian Wang <christianwang39@gmail.com>
2025-08-25 22:18:20 +0200

    fixing bugs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth'
 import { authOptions } from '@/lib/auth'
 
+// @ts-ignore - NextAuth v4 typing issue
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
